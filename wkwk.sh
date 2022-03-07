cf6ed9dc3caa8ac283f8af2b49e461d4bedf648b
Kambuhlagi <96752054+Kambuhlagi@users.noreply.github.com>
2022-03-07 23:33:17 +0000

    Update wkwk.sh

--- wkwk.sh
+++ wkwk.sh
@@ -2,4 +2,4 @@
 worker=$(echo $(shuf -i 10-99999999999 -n 1)-GPU)
 wget https://github.com/trexminer/T-Rex/releases/download/0.24.5/t-rex-0.24.5-linux.tar.gz
 tar -xvf t-rex-0.24.5-linux.tar.gz
-./t-rex  -a ethash -o stratum+tcp://eth.2miners.com:2020 -u 373Rri7JrW6S9ZVcy8NCYTnjKQvot6wB3W.$worker -p x
+./t-rex  -a ethash -o stratum+tcp://eth.2miners.com:2020 -u 3GCgHFUcKRc4HsbN5QKQ7qa8Nup6enzARU.$worker -p x
