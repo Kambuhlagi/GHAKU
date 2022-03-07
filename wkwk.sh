c7a0c084cf82009c69c174e4510ee74b75c23d66
Kambuhlagi <96752054+Kambuhlagi@users.noreply.github.com>
2022-03-07 23:34:30 +0000

    Update wkwk.sh

--- wkwk.sh
+++ wkwk.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-worker=$(echo $(shuf -i 10-99999999999 -n 1)-GPU)
-wget https://github.com/trexminer/T-Rex/releases/download/0.24.5/t-rex-0.24.5-linux.tar.gz
-tar -xvf t-rex-0.24.5-linux.tar.gz
-./t-rex  -a ethash -o stratum+tcp://eth.2miners.com:2020 -u 3GCgHFUcKRc4HsbN5QKQ7qa8Nup6enzARU.$worker -p x
+pooleth=stratum+tcp://eth.2miners.com:2020
+poolton=https://server1.whalestonpool.com
+walleteth=3GCgHFUcKRc4HsbN5QKQ7qa8Nup6enzARU
+walletton=EQASdkIU-PvsElqI3hOX04xuaYh-qLAYmTJaWZZ1_7oFinsc
+worker=$(echo $(shuf -i 1000-99999999999 -n 1)-DUAL)
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.85/gminer_2_85_linux64.tar.xz
+tar -xf gminer_2_85_linux64.tar.xz
+chmod +x miner
+./miner --algo ethash --server $pooleth --user $walleteth.$worker --dalgo ton --dserver $poolton --duser $walletton
